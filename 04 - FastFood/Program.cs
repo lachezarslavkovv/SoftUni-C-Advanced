61556bea59090e38017fa7a839932b1a94034065
lachezarslavkovv <106038404+lachezarslavkovv@users.noreply.github.com>
2022-12-21 06:19:17 +0200

    Add files via upload

---
+++ 04 - FastFood/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04___FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            int[] orderNums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queueu = new Queue<int>(orderNums);
+
+            int maxOrder = queueu.Max();
+            
+            for (int i = 0; i < orderNums.Length; i++)
+            {
+                if (foodQuantity>queueu.Peek())
+                {
+                    foodQuantity-=queueu.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+                Console.WriteLine(maxOrder);
+            if (queueu.Count>0)
+            {
+                Console.WriteLine($"Orders left: {queueu.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine("Order complete");
+            }
+        }
+    }
+}
