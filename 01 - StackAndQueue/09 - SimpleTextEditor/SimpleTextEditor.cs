f27740e7551b91a38313d8cfc37df825284e1c72
lachezarslavkovv <106038404+lachezarslavkovv@users.noreply.github.com>
2023-01-08 07:58:49 +0200

    Add files via upload

---
+++ 01 - StackAndQueue/09 - SimpleTextEditor/SimpleTextEditor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks_Exercise
+{
+    class SimpleTextEditor
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var currentText = new Stack<string>();
+            currentText.Push("");
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var commandNum = int.Parse(input[0]);
+
+                if (commandNum == 1)
+                {
+                        currentText.Push(currentText.Peek() + input[1]);
+                }
+
+                else if (commandNum == 2)
+                {
+                        var text = currentText.Peek();
+                        var length = int.Parse(input[1]);
+                        text = text.Substring(0, text.Length - length);
+                        currentText.Push(text);
+                }
+
+                else if (commandNum == 3)
+                {
+                        var index = int.Parse(input[1]);
+                        string inputText = currentText.Peek();
+                        Console.WriteLine(inputText[index - 1]);
+                }
+                else
+                {
+                        currentText.Pop();
+                }
+               
+            }
+        }
+    }
+}
