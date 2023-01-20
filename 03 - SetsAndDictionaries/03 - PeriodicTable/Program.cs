0c9d49a4030772182bfa64fe8042fda96f3ddcaa
lachezarslavkovv <106038404+lachezarslavkovv@users.noreply.github.com>
2023-01-20 09:31:19 +0200

    Add files via upload

---
+++ 03 - SetsAndDictionaries/03 - PeriodicTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03___PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            SortedSet<string> chemicalElements = new SortedSet<string>();
+            for (int i = 0; i < num; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+                foreach (var element in elements)
+                {
+                    chemicalElements.Add(element);
+                }
+            }
+
+            Console.WriteLine(String.Join(' ', chemicalElements));
+
+        }
+    }
+}
