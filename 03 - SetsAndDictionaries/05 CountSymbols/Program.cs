1b73a93522eecce2b15aff5b1b70a049d183b8f3
lachezarslavkovv <106038404+lachezarslavkovv@users.noreply.github.com>
2023-01-21 21:32:23 +0200

    Add files via upload

---
+++ 03 - SetsAndDictionaries/05 CountSymbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05_CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] symbols = input.ToCharArray();
+
+            SortedDictionary<char, int> symbolsDictionary = new SortedDictionary<char, int>();
+            
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                if (symbolsDictionary.ContainsKey(symbols[i]))
+                {
+                    symbolsDictionary[symbols[i]]++;
+                }
+                else
+                {
+                    symbolsDictionary.Add(symbols[i], 1);
+                }
+            }
+            foreach (var item in symbolsDictionary)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
