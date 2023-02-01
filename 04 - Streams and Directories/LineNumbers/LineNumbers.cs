a70a9996b26c73be3dc19d3de2bb61874db2d62d
lachezarslavkovv <106038404+lachezarslavkovv@users.noreply.github.com>
2023-02-01 23:49:57 +0200

    Add files via upload

---
+++ 04 - Streams and Directories/LineNumbers/LineNumbers.cs
@@ -0,0 +1,29 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using StreamReader reader = new StreamReader(inputFilePath);
+            using StreamWriter writer = new StreamWriter(outputFilePath);
+            int number = 1;
+
+            while (!reader.EndOfStream)
+            {
+                string text = reader.ReadLine();
+
+                writer.WriteLine($"{number}. {text}");
+                number++;
+            }
+        }
+    }
+}
