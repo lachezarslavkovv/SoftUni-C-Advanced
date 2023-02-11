fd5ad9de56f48e32b27ca39df835180ef280cb87
lachezarslavkovv <106038404+lachezarslavkovv@users.noreply.github.com>
2023-02-11 23:25:05 +0200

    Add files via upload

---
+++ 04 - Streams and Directories/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelCons = double.Parse(Console.ReadLine());
+
+            Car firstCar = new Car();
+            Car secondCar = new Car(make, model, year);
+            Car thiurdCar = new Car(make, model, year, fuelQuantity, fuelCons);
+
+
+            Console.WriteLine(firstCar.WhoAmI());
+            Console.WriteLine(secondCar.WhoAmI());
+            Console.WriteLine(thiurdCar.WhoAmI());
+
+        }
+    }
+}
